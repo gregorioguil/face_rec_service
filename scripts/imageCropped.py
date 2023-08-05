46f6abda828bacc77cbec12cd7c007b751f87dc1
Guilherme Gregorio <ggregorio666@hotmail.com>
2023-08-05 11:34:56 -0300

    chore: remove images

--- scripts/imageCropped.py
+++ scripts/imageCropped.py
@@ -9,14 +9,14 @@ print(files_sub)
 face_cascade = cv2.CascadeClassifier('../models/haarcascade_frontalface_alt2.xml')
 for i in range(len(files_sub)):
     files = os.listdir(path_original+"/"+files_sub[i])
-    k = 1;
+    k = 1
     for j in range(len(files)):
         k = k * 10
         try:
             img = cv2.imread(path_original+"/"+files_sub[i]+"/"+files[j])
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.3, 4)
-            print(faces)
+
             for(x,y,w,h) in faces:
                 roi_color = img[y:y+h, x:x+w]
                 print(path_destination+"/"+files_sub[i]+"/"+files[j])
